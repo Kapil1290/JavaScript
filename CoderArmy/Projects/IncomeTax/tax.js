123f0591709619536736d595a21d651856e302cb
Kapil1290 <kapilprajapat9303@gmail.com>
2026-02-12 22:21:36 +0530

    income tax project with events

--- CoderArmy/Projects/IncomeTax/tax.js
+++ CoderArmy/Projects/IncomeTax/tax.js
@@ -4,14 +4,20 @@ form.addEventListener('submit',(e)=>{
     e.preventDefault()
     let a = Number(income.value);
     let tax;
-    if(a>1200000  &&  a<1600000){
+    if(a<=1200000){
+        tax ="No Tax";
+    }else if(a>1200000  &&  a<1600000){
         a=a-1200000;
         tax = (a/100)*15;
-    } else if(a>=1600000){
+    } else if(a>=1600000 && a<2000000){
         a=a-1200000;
         tax = (a/100)*20;
-    } else{
-        tax ="No Tax";
+    } else if(a>=2000000 && a<2400000){
+        a=a-1200000
+        tax = (a/100)*25;
+    }else{
+        a=a-1200000
+        tax = (a/100)*30;
     }
     document.getElementById("res").textContent = `Total Tax: ${tax}%`;
     form.reset()
