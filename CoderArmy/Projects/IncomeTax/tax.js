cff013df94ee834ddc5d3f477d2faf2198a492bb
Kapil1290 <kapilprajapat9303@gmail.com>
2026-02-13 18:18:35 +0530

    tax case modificationi

--- CoderArmy/Projects/IncomeTax/tax.js
+++ CoderArmy/Projects/IncomeTax/tax.js
@@ -4,20 +4,10 @@ form.addEventListener('submit',(e)=>{
     e.preventDefault()
     let a = Number(income.value);
     let tax;
-    if(a<1200000){
-        tax ="No Tax";
-    }else if(a>=1200000  &&  a<1600000){
-        a=a-1200000;
-        tax = (a/100)*15;
-    } else if(a>=1600000 && a<2000000){
-        a=a-1200000;
-        tax = (a/100)*20;
-    } else if(a>=2000000 && a<2400000){
-        a=a-1200000
-        tax = (a/100)*25;
-    }else{
-        a=a-1200000
-        tax = (a/100)*30;
+    if(a<=1200000){
+        tax = "No Tax Hurahhhh...";
+    } else if(a<=1600000){
+        tax = (a-1600000) 
     }
     document.getElementById("res").textContent = `Total Tax: ${tax}%`;
     form.reset()
