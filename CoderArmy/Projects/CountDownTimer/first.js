9221876b9a0476425c55399aa87949eb6189ff25
Kapil1290 <kapilprajapat9303@gmail.com>
2026-02-13 21:33:07 +0530

    project done of count down

--- CoderArmy/Projects/CountDownTimer/first.js
+++ CoderArmy/Projects/CountDownTimer/first.js
@@ -1,7 +1,23 @@
-const currentTime = Date.now();
+setInterval(()=>{
+    const currentTime = Date.now();
 //for current time 
 // console.log(currentTime)
 //olympic time 
 const olympicTime = new Date(2028,6,23).getTime();
-const remainTime = olympicTime-currentTime;
-let day = (remainTime)/(1000*60*60*24)
+let remainTime = olympicTime-currentTime;
+
+const day = Math.floor((remainTime)/(1000*60*60*24))
+remainTime%=1000*60*60*24;
+
+let hour = Math.floor((remainTime)/(1000*60*60))
+remainTime%=1000*60*60;
+
+let min = Math.floor((remainTime)/(1000*60))
+remainTime%=1000*60;
+
+let sec = Math.floor((remainTime)/(1000))
+remainTime%=1000;
+
+const timer = document.getElementById("timer");
+timer.textContent = `${day}days ${hour}hours ${min}min ${sec}sec`
+})
