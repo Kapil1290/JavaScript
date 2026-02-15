197a2f7786d3092c71bfb8a20d03536937d16288
Kapil1290 <kapilprajapat9303@gmail.com>
2026-02-15 15:42:50 +0530

    adding css

--- CoderArmy/Projects/Clicker/logic.js
+++ CoderArmy/Projects/Clicker/logic.js
@@ -1,6 +1,5 @@
 const body = document.querySelector('body');
 
 body.addEventListener('click',(e)=>{
-    e.preventDefault();
-    console.log(e.clientX,e.clientY);
+    console.log(e.clientX , e.clientY);
 })
