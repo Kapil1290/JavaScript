f35c29909f6659488df4c316144148a36d267790
Kapil1290 <kapilprajapat9303@gmail.com>
2026-02-15 15:55:36 +0530

    almost done

--- CoderArmy/Projects/Clicker/logic.js
+++ CoderArmy/Projects/Clicker/logic.js
@@ -2,4 +2,12 @@ const body = document.querySelector('body');
 
 body.addEventListener('click',(e)=>{
     console.log(e.clientX , e.clientY);
+
+    const createElement = document.createElement("div");
+    createElement.classList.add('circle');
+    let color = ['red', 'blue', 'yellow', 'pink', 'purple', 'aqua', 'brown'];
+    createElement.style.backgroundColor = color[Math.floor(Math.random()*6)];
+
+    createElement.style.top = `${e.clientY}`;
+    createElement.style.left = `${e.clientX}`;
 })
