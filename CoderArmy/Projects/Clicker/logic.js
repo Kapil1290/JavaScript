37d03cd24d5f1dd836df937f9ff38585ab17c005
Kapil1290 <kapilprajapat9303@gmail.com>
2026-02-15 16:19:27 +0530

    finally done

--- CoderArmy/Projects/Clicker/logic.js
+++ CoderArmy/Projects/Clicker/logic.js
@@ -7,7 +7,7 @@ body.addEventListener('click',(e)=>{
     let color = ['red', 'blue', 'yellow', 'pink', 'purple', 'aqua', 'brown'];
     createElement.style.backgroundColor = color[Math.floor(Math.random()*6)];
     createElement.textContent = "Hii";
-    createElement.style.left = `${e.clientX}px`;
-    createElement.style.top = `${e.clientY}px`;
+    createElement.style.left = `${e.clientX-25}px`;
+    createElement.style.top = `${e.clientY-25}px`;
     body.append(createElement);
 })
