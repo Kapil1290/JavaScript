efc9adb1ad4e23f0e0d5158d077558de73592dea
Kapil1290 <kapilprajapat9303@gmail.com>
2026-02-16 12:59:36 +0530

    new project working

--- CoderArmy/Projects/To Do List/first.js
+++ CoderArmy/Projects/To Do List/first.js
@@ -1,10 +1,40 @@
 const form = document.querySelector("form");
-const input = document.querySelector('input');
-const task = document.getElementById("task");
+const alltask = document.getElementById("all");
+const input = document.querySelector("input");
 
 form.addEventListener('submit',(e)=>{
-    e.preventDefault();
-  const val = input.value;
-  task.append(val+" \n");
-    form.reset()
+  e.preventDefault()
+  const text = input.value;
+  if(text=="")
+    return;
+
+  const parent = document.createElement("div");
+  parent.style.marginTop = "10px"
+  const span = document.createElement('span');
+  span.textContent = text;
+  span.style.marginRight = "20px";
+
+
+  const deleteButton = document.createElement('button');
+  deleteButton.textContent = "Delete";
+  deleteButton.style.width = "60px"
+
+  const doneButton = document.createElement('button');
+  doneButton.textContent = "Done";
+  doneButton.style.width = "60px";
+  doneButton.style.marginRight = "10px"
+  doneButton.style.marginLeft = "10px";
+  
+  parent.append(span,doneButton,deleteButton)
+  alltask.append(parent)
+
+  form.reset()
+  doneButton.addEventListener('click',(e)=>{
+    span.style.textDecoration = "line-through";
+    span.style.opacity = "0.6";
+  })
+
+  deleteButton.addEventListener('click',(e)=>{
+    parent.remove()
+  })
 })
