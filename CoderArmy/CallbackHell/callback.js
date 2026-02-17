8dcf926694928f57c0826993e7568565a713a4a5
Kapil1290 <kapilprajapat9303@gmail.com>
2026-02-17 21:41:15 +0530

    understanding done

--- CoderArmy/CallbackHell/callback.js
+++ CoderArmy/CallbackHell/callback.js
@@ -7,7 +7,7 @@ const orderDetails ={
     orderId: 12131,
     food: ["Pizza","coke","Burger"],
     restorant_loc: "LIG indore"
-}
+};
 
 function paymentOrder(orderDetails, callback){
     console.log("Payment is in process");
@@ -28,7 +28,7 @@ function preparingOrder(orderDetails,callback){
 }
 
 function pickOrder(orderDetails, callback){
-    console.log(`Delivery boy is on the way to pick the order from ${restorant_loc}`)
+    console.log(`Delivery boy is on the way to pick the order from ${orderDetails.restorant_loc}`)
     setTimeout(() => {
         console.log("Order has picked by delivery boy");
         orderDetails.fetch_delivery_from_resto = true;
@@ -36,18 +36,21 @@ function pickOrder(orderDetails, callback){
     }, 2000);
 }
 
-function deliveryOrder(){
+function deliveryOrder(callback){
     console.log("Order has dispatched");
     setTimeout(() => {
         console.log("order successfully arrived")
     }, 2000);
 }
 
+const callback = ()=>{
+    deliveryOrder(orderDetails);
+}
 
-paymentOrder(orderDetails,(orderDetails)=>{
-    preparingOrder(orderDetails,(orderDetails)=>{
-        pickOrder(orderDetails,()=>{                  // we are passing function 
-            deliveryOrder();                // callback inside function
-        })
-    });
-});
+// paymentOrder(orderDetails,(orderDetails)=>{
+//     preparingOrder(orderDetails,(orderDetails)=>{
+//         pickOrder(orderDetails,()=>{                  // we are passing function 
+//             deliveryOrder();                // callback inside function
+//         })
+//     });
+// });
