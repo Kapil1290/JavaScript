586e1409e568b9f0cef0aad295ddc6aa522dee93
Kapil1290 <kapilprajapat9303@gmail.com>
2026-02-22 11:52:24 +0530

    play with objects

--- CoderArmy/Prototype/index.js
+++ CoderArmy/Prototype/index.js
@@ -9,5 +9,18 @@ const obj = {
 
 // console.log(obj.name);
 
-console.log(obj.greet())
-console.log(obj.hasOwnProperty("name"))
+// console.log(obj.greet())
+// console.log(obj.hasOwnProperty("name"))
+
+console.log(obj.toString())
+
+
+const obj1 = {
+    account:23,
+    // name:"Tarun"
+}
+
+obj1.__proto__=obj;
+
+console.log(obj1.name)
+console.log(obj1.hasOwnProperty("name"))
