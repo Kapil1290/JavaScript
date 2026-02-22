1215b3496bf25ffbd2812cfc59f58293e9b96fee
Kapil1290 <kapilprajapat9303@gmail.com>
2026-02-22 11:16:40 +0530

    i have learned async funtion

--- CoderArmy/Async-Await/index.js
+++ CoderArmy/Async-Await/index.js
@@ -23,118 +23,118 @@
 // })
 
 
-async function git(){
-    try {
-    const response = await fetch("https://api.github.com/users")
-    const info = await response.json()
-    if(!response.ok){                                           // if url is wrong then response.ok will become false--> true
-        throw new Error("Error inside the server")
-    }
+// async function git(){
+//     try {
+//     const response = await fetch("https://api.github.com/users")
+//     const info = await response.json()
+//     if(!response.ok){                                           // if url is wrong then response.ok will become false--> true
+//         throw new Error("Error inside the server")
+//     }
     
-    const div = document.getElementById("first")
-    for(let i=0; i<info.length; i++){
+//     const div = document.getElementById("first")
+//     for(let i=0; i<info.length; i++){
         
-        const innerdiv = document.createElement('div');
-        innerdiv.classList.add("user");
+//         const innerdiv = document.createElement('div');
+//         innerdiv.classList.add("user");
 
-        const image = document.createElement('img');
-        image.src = info[i].avatar_url;
+//         const image = document.createElement('img');
+//         image.src = info[i].avatar_url;
         
-        const name = document.createElement('h2');
-        name.textContent = info[i].login;
+//         const name = document.createElement('h2');
+//         name.textContent = info[i].login;
 
-        const anchor = document.createElement('a');
-        anchor.href = info[i].url;
-        anchor.textContent = "Visit Profile";
+//         const anchor = document.createElement('a');
+//         anchor.href = info[i].url;
+//         anchor.textContent = "Visit Profile";
 
-        innerdiv.append(image,name,anchor)
+//         innerdiv.append(image,name,anchor)
 
-        div.append(innerdiv);
-    }
-    } catch (error) {
-        console.log("Error: ",error)
-    }
+//         div.append(innerdiv);
+//     }
+//     } catch (error) {
+//         console.log("Error: ",error)
+//     }
+// }
+
+// git();
+
+
+
+
+
+
+
+const orderDetails ={
+    customer_name: "Kapil",
+    customer_loc: "NewYork Sage Indore",
+    cost: 350,
+    orderId: 12131,
+    food: ["Pizza","coke","Burger"],
+    restorant_loc: "LIG indore"
+};
+
+function paymentOrder(orderDetails){
+    console.log("Payment is in process");
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+        if(Math.random()>0.9){
+            console.log(`${orderDetails.cost}Rs Payment has received and order placed`);
+        resolve(orderDetails)
+        }else{
+            reject("Payment Failed! Try later")
+        }
+    }, 4000)
+    
+    })
+    
 }
 
-git();
-
-
-
-
-
-
-
-// const orderDetails ={
-//     customer_name: "Kapil",
-//     customer_loc: "NewYork Sage Indore",
-//     cost: 350,
-//     orderId: 12131,
-//     food: ["Pizza","coke","Burger"],
-//     restorant_loc: "LIG indore"
-// };
-
-// function paymentOrder(orderDetails){
-//     console.log("Payment is in process");
-//     return new Promise((resolve,reject)=>{
-//         setTimeout(()=>{
-//         if(Math.random()>0.9){
-//             console.log(`${orderDetails.cost}Rs Payment has received and order placed`);
-//         resolve(orderDetails)
-//         }else{
-//             reject("Payment Failed! Try later")
-//         }
-//     }, 4000)
+function preparingOrder(orderDetails){
+    console.log("Order is preparing");
+    return new Promise((resolve,reject)=>{
+        setTimeout(() => {
+            if(Math.random()>0.1){
+                console.log("Order has prepared");
+        orderDetails.preparing_status = true;
+        resolve(orderDetails)
+            }else{
+                reject("Order not accepted right now")
+            }
+    }, 3000);
     
-//     })
-    
-// }
+    })
+}
 
-// function preparingOrder(orderDetails){
-//     console.log("Order is preparing");
-//     return new Promise((resolve,reject)=>{
-//         setTimeout(() => {
-//             if(Math.random()>0.1){
-//                 console.log("Order has prepared");
-//         orderDetails.preparing_status = true;
-//         resolve(orderDetails)
-//             }else{
-//                 reject("Order not accepted right now")
-//             }
-//     }, 3000);
+function pickOrder(orderDetails){
+    console.log(`Delivery boy is on the way to pick the order from ${orderDetails.restorant_loc}`)
+    return new Promise((resolve,reject)=>{
+        setTimeout(() => {
+            if(Math.random()>0.3){
+                console.log("Order has picked by delivery boy");
+        orderDetails.fetch_delivery_from_resto = true;
+        resolve(orderDetails)
+            }else{
+                reject("Deliver failed due to some reason!!!!!!!")
+            }
+    }, 2000);
     
-//     })
-// }
+    })
+}
 
-// function pickOrder(orderDetails){
-//     console.log(`Delivery boy is on the way to pick the order from ${orderDetails.restorant_loc}`)
-//     return new Promise((resolve,reject)=>{
-//         setTimeout(() => {
-//             if(Math.random()>0.3){
-//                 console.log("Order has picked by delivery boy");
-//         orderDetails.fetch_delivery_from_resto = true;
-//         resolve(orderDetails)
-//             }else{
-//                 reject("Deliver failed due to some reason!!!!!!!")
-//             }
-//     }, 2000);
+function deliveryOrder(orderDetails){
+    console.log("Order has dispatched");
+    return new Promise((resolve,reject)=>{
+        setTimeout(() => {
+            if(Math.random()>0.1){
+                console.log("order successfully arrived")
+        resolve(orderDetails)
+            }else{
+                reject("Order not succesfully arrived:)")
+            }
+    }, 2000);
     
-//     })
-// }
-
-// function deliveryOrder(callback){
-//     console.log("Order has dispatched");
-//     return new Promise((resolve,reject)=>{
-//         setTimeout(() => {
-//             if(Math.random()>0.1){
-//                 console.log("order successfully arrived")
-//         resolve(orderDetails)
-//             }else{
-//                 reject("Order not succesfully arrived:)")
-//             }
-//     }, 2000);
-    
-//     })
-// }
+    })
+}
 
 
 // // paymentOrder(orderDetails)
@@ -149,16 +149,28 @@ git();
 // // })
 
 
-// async function ordering() {
-//     try {
-//     const res1 = paymentOrder(orderDetails)
-//     const res2 = preparingOrder(res1);
-//     const res3 = preparingOrder(res2);
-//     const res4 = preparingOrder(res3);
-//     console.log(res4);
+async function ordering() {
+    try {
+    const res1 = await paymentOrder(orderDetails)
+    const res2 = await preparingOrder(res1);
+    const res3 = await pickOrder(res2);
+    const res4 = await deliveryOrder(res3);
+    console.log(res4);
 
-//     } catch (error) {
-//         console.log("Error",error)
-//     }
-// }
+    } catch (error) {
+        console.log("Error",error)
+    }
+}
+
+ordering()
+
+
+
+async function user(){
+    // const comment = await fetch("userComments");
+    // const photo = await fetch("userPhote")
+    // const userChat = await fetch("userChat")       // it all resolve one-by-one
+
+    const [comment,photo,chats]=await Promise.all([fetch("userComment"), fetch("userPhoto"), fetch("userChats")])  // for parallel execution on the api's
+}
 
