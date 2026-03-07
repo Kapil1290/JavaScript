79641a4011afc2172ca3ba809705c9290db69dad
Kapil1290 <kapilprajapat9303@gmail.com>
2026-03-07 12:29:33 +0530

    array learning

--- CoderArmy/Note2.js
+++ CoderArmy/Note2.js
@@ -48,7 +48,7 @@ console.log(val1)    //both same because they refer same reference in memory
 // console.log(numArr)
 
 
-const arr1 = [12,44,54,32,54]
+// const arr1 = [12,44,54,32,54]
 // const arr2 = ["kapil",23,33]
 // const arr4 = ["naman",34,false]
 // arr1.push(arr2)
@@ -56,37 +56,37 @@ const arr1 = [12,44,54,32,54]
 // const arr3 = arr1.concat(arr2)  // add two arrays
 
 
-//spread operator
-const arr3 = [...arr1,...arr2,...arr4]  // it spread and show
-console.log(arr3)
+// //spread operator
+// const arr3 = [...arr1,...arr2,...arr4]  // it spread and show
+// console.log(arr3)
 
-const names = ["Alice","Bob","Carni","Carni", "Shraddha", "Rohit", "alina"]
-console.log(names.toString())
-console.log(names.join(" # "))
+// const names = ["Alice","Bob","Carni","Carni", "Shraddha", "Rohit", "alina"]
+// console.log(names.toString())
+// console.log(names.join(" # "))
 
-console.log(names.indexOf("Carni"))
-console.log(names.lastIndexOf("Carni"))
-console.log(names.sort())  //work on actual array and use ASCII values for arrangements
-console.log(names.reverse()) // reverse the array
+// console.log(names.indexOf("Carni"))
+// console.log(names.lastIndexOf("Carni"))
+// console.log(names.sort())  //work on actual array and use ASCII values for arrangements
+// console.log(names.reverse()) // reverse the array
 
-const a=[23,22,101,3022]  // sort() treats like string (" ") on the basis of ASCII
-a.sort();
-console.log(a)
+// const a=[23,22,101,3022]  // sort() treats like string (" ") on the basis of ASCII
+// a.sort();
+// console.log(a)
 
-a.sort((a,b)=>a-b)  // To aarange in increasing order
-// -ve: pehle a aayega fr b aayega
-// +ve: pehle b aayega fr a aayega
-a.sort((a,b)=>b-a)
-console.log(a)
-
-
-const b = [23,22,54,[32,12,[34,33,55],11],90,34,54,[34,54,65,55]]
+// a.sort((a,b)=>a-b)  // To aarange in increasing order
+// // -ve: pehle a aayega fr b aayega
+// // +ve: pehle b aayega fr a aayega
+// a.sort((a,b)=>b-a)
+// console.log(a)
 
 
-console.log(b[3][1])
-console.log(b[3][3])
-console.log(b[7][2])
+// const b = [23,22,54,[32,12,[34,33,55],11],90,34,54,[34,54,65,55]]
 
 
-let newX = b.flat(1)  //this method de-bracketize the values and open by flat parameter like 0,1,2....infinity
-console.log(newX)
+// console.log(b[3][1])
+// console.log(b[3][3])
+// console.log(b[7][2])
+
+
+// let newX = b.flat(1)  //this method de-bracketize the values and open by flat parameter like 0,1,2....infinity
+// console.log(newX)
